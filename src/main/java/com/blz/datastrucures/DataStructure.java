597cd7568c5850fce5229b0d988b2c06ca22794d
sandeepvarma8466 <svarma852@gmail.com>
2022-07-08 13:12:12 +0530

    Welcome to DataStructure program

---
+++ src/main/java/com/blz/datastrucures/DataStructure.java
@@ -0,0 +1,7 @@
+package com.blz.datastrucures;
+
+public class DataStructure {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Data Structures Problem");
+    }
+}
